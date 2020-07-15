26cb74b0155ca1bac434ff3ec42a6f8b2c0d25c1
Shingo Hiraya <shingohiraya@gmail.com>
2020-07-16 00:27:57 +0900

    DoublyLlinkedListクラスを追加

---
+++ LRUCache/Cache/DoublyLlinkedList.swift
@@ -0,0 +1,15 @@
+//
+//  DoublyLlinkedList.swift
+//  LRUCache
+//
+//  Created by Shingo Hiraya on 2020/07/16.
+//  Copyright © 2020 Shingo Hiraya. All rights reserved.
+//
+
+import Foundation
+
+class DoublyLlinkedList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    var size = 0
+}
